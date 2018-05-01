e62ce8eb628ec8c5fb50bcdee0a1a3a113691365
maartje <maartjedejonge@gmail.com>
2018-05-01 09:26:08 +0200

    store AER and llhood results

--- persistence.py
+++ persistence.py
@@ -6,3 +6,12 @@ def store_ibm1_model(lprobs, fname):
 def load_ibm1_model(fname):
     lprobs = json.load(open(fname))
     return lprobs
+
+def store_iteration_results(scores, fname):
+    with open(fname, "w") as file:
+        file.write(str(scores))
+
+def load_iteration_results(fname):
+    with open(fname, "r") as file:
+        scores = eval(file.readline())
+    return scores
