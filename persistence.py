882e4dd02a43c89a56e96a87a2217bbaea48faf4
maartje <maartjedejonge@gmail.com>
2018-05-02 05:56:48 +0200

    IBM2 ran successfully

--- persistence.py
+++ persistence.py
@@ -7,9 +7,20 @@ def load_ibm1_model(fname):
     lprobs = json.load(open(fname))
     return lprobs
 
+def save_ibm2_model(lprobs, jump_probs, fname):
+    ibm2_model = {
+        'lprobs' : lprobs, 
+        'jump_probs' : jump_probs
+    }
+    json.dump(ibm2_model, open(fname,'w'))
+
+def load_ibm2_model(fname):
+    ibm2_model =  json.load(open(fname))
+    return (ibm2_model['lprobs'], ibm2_model['jump_probs'])
+
 def save(data, fname):
     with open(fname, "w") as file:
-        file.write(str(scores))
+        file.write(str(data))
 
 def load(fname):
     with open(fname, "r") as file:
