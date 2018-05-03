9fab5c637574ae5355dd467ef60096f284d10a61
maartje <maartjedejonge@gmail.com>
2018-05-03 11:58:31 +0200

    clean up

--- plots.py
+++ plots.py
@@ -3,7 +3,7 @@ from matplotlib.pyplot import figure, show
 from matplotlib.ticker import MaxNLocator
 
 # save figure training log likelihood vs iteration
-def figure_log_likelihood(log_lhoods, model_name=None):
+def figure_log_likelihood(log_lhoods, fname=None):
     ax = figure().gca()
     _ = plt.plot(log_lhoods)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
@@ -14,12 +14,12 @@ def figure_log_likelihood(log_lhoods, model_name=None):
     plt.yticks(fontsize=20)
     _ = plt.ylabel('LL-hood', fontsize=20, fontstyle = 'oblique')
     _ = plt.xlabel('iteration', fontsize=20, fontstyle = 'oblique')
-    if model_name:
-         _ = plt.savefig(f'{model_name}_llhood.png')
+    if fname:
+         _ = plt.savefig(fname)
     _ = plt.show()
     
 # save figure validation AER vs iteration
-def figure_AER(AERs, model_name = None):
+def figure_AER(AERs, fname = None):
     ax = figure().gca()
     _ = plt.plot(AERs)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
@@ -29,11 +29,11 @@ def figure_AER(AERs, model_name = None):
     plt.yticks(fontsize=20)
     _ = plt.ylabel('AER', fontsize=20, fontstyle = 'oblique')
     _ = plt.xlabel('iteration', fontsize=20, fontstyle = 'oblique')
-    if model_name:
-        _ = plt.savefig(f'{model_name}_AER.png')
+    if fname:
+        _ = plt.savefig(fname)
     _ = plt.show()
 
-def figure_llhood_multiple_lines(llhood_lines, line_labels, model_name=None):
+def figure_llhood_multiple_lines(llhood_lines, line_labels, fname = None):
     ax = figure().gca()
     for i, llhoods in enumerate(llhood_lines):
         _ = plt.plot(llhoods, label = line_labels[i])
@@ -46,11 +46,11 @@ def figure_llhood_multiple_lines(llhood_lines, line_labels, model_name=None):
     _ = plt.ylabel('LL-hood', fontsize=20, fontstyle = 'oblique')
     _ = plt.xlabel('iteration', fontsize=20, fontstyle = 'oblique')
     plt.legend()
-    if model_name:
-         _ = plt.savefig(f'{model_name}_llhood.png')
+    if fname:
+         _ = plt.savefig(fname)
     _ = plt.show()
 
-def figure_AER_multiple_lines(AER_lines, line_labels, model_name=None):
+def figure_AER_multiple_lines(AER_lines, line_labels, fname = None):
     ax = figure().gca()
     for i, AERs in enumerate(AER_lines):
         _ = plt.plot(AERs, label = line_labels[i])
@@ -62,7 +62,7 @@ def figure_AER_multiple_lines(AER_lines, line_labels, model_name=None):
     _ = plt.ylabel('AER', fontsize=20, fontstyle = 'oblique')
     _ = plt.xlabel('iteration', fontsize=20, fontstyle = 'oblique')
     plt.legend()
-    if model_name:
-        _ = plt.savefig(f'{model_name}_AER.png')
+    if fname:
+        _ = plt.savefig(fname)
     _ = plt.show()
 
